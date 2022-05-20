1464a77c6549d3e00a9c34731568db5466885b87
Kaffar <kaffario@gmail.com>
2022-05-20 20:30:01 +0300

    lesson #31

---
+++ 018resultcode/src/main/java/com/example/a018resultcode/AlignActivity.java
@@ -0,0 +1,14 @@
+package com.example.a018resultcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.align);
+    }
+}
---
+++ 018resultcode/src/main/java/com/example/a018resultcode/ColorActivity.java
@@ -0,0 +1,14 @@
+package com.example.a018resultcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color);
+    }
+}
