8b3fdcf49c308f5ab1686d971e57004d416c9c2e
Kaffar <kaffario@gmail.com>
2022-05-24 22:04:29 +0300

    #33

---
+++ 021simplebrowser/src/main/java/com/example/a021simplebrowser/BrowserActivity.java
@@ -0,0 +1,14 @@
+package com.example.a021simplebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browser);
+    }
+}
