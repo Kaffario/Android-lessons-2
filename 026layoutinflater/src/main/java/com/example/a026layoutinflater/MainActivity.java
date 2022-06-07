bcdb82e5c48698193ba225dccf338f48a10792c4
Kaffar <kaffario@gmail.com>
2022-06-07 11:55:47 +0300

    #42

---
+++ 026layoutinflater/src/main/java/com/example/a026layoutinflater/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a026layoutinflater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        View view1 = ltInflater.inflate(R.layout.text, linLayout, true);
+        LayoutParams lp1 = view1.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        View view2 = ltInflater.inflate(R.layout.text, relLayout, true);
+        LayoutParams lp2 = view2.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
+    }
+}
---
+++ 028simplelist/src/main/java/com/example/a028simplelist/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a028simplelist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+
+    }
+}
